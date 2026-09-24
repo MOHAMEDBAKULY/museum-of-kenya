159d237a5b6ecf6af851ca3a3109830f1c74fdd8
mohamed bakuly <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2026-09-24 08:11:51 +0000

    Add Vercel Analytics.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import '@fontsource/instrument-serif/400.css'
 import '@fontsource/instrument-serif/400-italic.css'
 import '@fontsource-variable/inter'
+import { Analytics } from '@vercel/analytics/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -9,5 +10,6 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
